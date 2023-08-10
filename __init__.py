7a1c7c134279d8540d3646c24cc225a7a2c863ba
Jordan Thompson <jordanslost@gmail.com>
2023-08-10 12:25:00 -0700

    Initial (broken) commit

---
+++ __init__.py
@@ -0,0 +1,11 @@
+from .nodes import Preset_Model_Merge
+
+NODE_CLASS_MAPPINGS = {
+    "Preset_Model_Merge": Preset_Model_Merge
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "Preset_Model_Merge": "MergeModelPreset"
+}
+
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
